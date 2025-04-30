99d170d5d4f4a57b4dda38882e1566db2713e069
Pelayogm <pelagrmenendez12@gmail.com>
2025-04-30 21:21:44 +0200

    SQLite primera implementacion 30/04/2025

---
+++ BBDD/BBDDManager.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.IO;
+using System.Data;
+using Mono.Data.Sqlite;
+
+namespace Funciones.BBDD
+{
+    public class BBDDManager : MonoBehaviour
+    {
+        private string _connectionString;
+
+        private void Start()
+        {
+            string dbName = "Clasificacion.db";
+            string sourcePath = Path.Combine(Application.streamingAssetsPath, dbName);
+            string targetPath = Path.Combine(Application.persistentDataPath, dbName);
+            
+            if (!File.Exists(targetPath))
+            {
+                File.Copy(sourcePath, targetPath);
+            }
+            
+            _connectionString = "URI=file:" + targetPath;
+
+            GetPuntuaciones();
+        }
+
+        private void GetPuntuaciones()
+        {
+            using (IDbConnection dbConexion = new SqliteConnection(_connectionString))
+            {
+                dbConexion.Open();
+                using (IDbCommand comando = dbConexion.CreateCommand())
+                {
+                    comando.CommandText = "SELECT * FROM CLASIFICACION";
+                    using (IDataReader lector = comando.ExecuteReader())
+                    {
+                        while (lector.Read())
+                        {
+                            Debug.Log(lector.GetString(3));
+                        }
+                        
+                        dbConexion.Close();
+                        lector.Close();
+                    }
+                }
+            }
+        }
+    }
+}
